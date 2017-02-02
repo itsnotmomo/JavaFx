a96d38d3fa0f5dca0d02aedf5fa7b217dee76671
Studente <Studente@Labinf16.belluzzifioravanti.local>
2017-02-02 09:35:07 +0100

    Merge origin/master

---
+++ src/login/grid.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package login;
+
+import javafx.scene.layout.HBox;
+
+/**
+ *
+ * @author Studente
+ */
+class grid {
+
+    static void add(HBox hbBtn, int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
